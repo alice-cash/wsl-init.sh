2a21eab7cba875a105216c98079b17fd9ed6f5b7
masshuku <supermatthew2@gmail.com>
2019-03-02 05:30:48 -0600

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Add new shortcut under the following directory
+# "C:\Users\%USERAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+# Make the shortcut here the following is an example. You can change the 3 to 5 for example if for example you used x11
+# C:\Windows\System32\bash.exe -c "/usr/bin/sudo /etc/init.sh 3"
+
+[[ -z $1 ]] && { echo "Need to specify a run level"; exit 127; }
+
+run_level=$1
+
+[[ -d /etc/rc${run_level}.d/ ]] ||  { echo "Need to specify a run level"; exit 127; }
+
+for rc_service in /etc/rc${run_level}.d/K*; do
+        $rc_service stop
+done
+
+for rc_service in /etc/rc${run_level}.d/S*; do
+        $rc_service start
+done
