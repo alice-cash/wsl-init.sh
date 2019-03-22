5ed441db10b7728655c7122ec5564916df9c7551
masshuku <supermatthew2@gmail.com>
2019-03-22 05:45:35 -0500

    Update and rename init.sh to wsl-init.sh

--- wsl-init.sh
+++ wsl-init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Add new shortcut under the following directory
-# "C:\Users\%USERAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-# Make the shortcut here the following is an example, assuming this file is /etc/init.sh
+# "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+# Make the shortcut here the following is an example, assuming this file is /etc/wsl-init.sh
 # You can change the 3 to 5 for example if for example you used x11
-# C:\Windows\System32\bash.exe -c "/usr/bin/sudo /etc/init.sh 3"
+# C:\Windows\System32\bash.exe -c "/usr/bin/sudo /etc/wsl-init.sh 3"
 
 [[ -z $1 ]] && { echo "Need to specify a run level"; exit 127; }
 
